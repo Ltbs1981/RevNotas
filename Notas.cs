137c0f19d8231ee21658323307efed6bbb3c108e
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-28 15:45:34 -0300

    pronto

---
+++ Notas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevNotas
+{
+    public class Notas
+    {
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public Notas(double nota1, double nota2, double nota3)
+        {
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+        public double MediaNotas()
+        {
+            return (Nota1  * 0.30)+ (Nota2 * 0.35) + (Nota3 * 0.35) / 3;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RevNotas;
+
+Console.WriteLine("Calculando médias");
+
+Notas notas = new Notas(8.5, 1.9, 5.8);
+
+double notaFinal = notas.MediaNotas();
+
+Console.WriteLine($"Notal final: {notaFinal:f2}");
+
+if (notaFinal < 6.0)
+{
+    Console.WriteLine("Reprovado");
+    double pontos = 6.0 - notaFinal;
+    Console.WriteLine($"Faltam {pontos:f2} pontos para você ser aprovado");
+}else
+{
+    Console.WriteLine("Parabéns! Aprovado.");
+}
