dad6519e4f8f0b1770e1c7f3479c6b881c427b27
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-05 13:32:13 -0300

    propriedades criadas

--- Notas.cs
+++ Notas.cs
@@ -8,26 +8,10 @@ namespace RevNotas
 {
     public class Notas
     {
-        private double _nota1;
-        public double Nota1
-        {
-            get { return _nota1; }
-            set { _nota1 = value; }
-        }
+        public double  Nota1 { get; set; }
+        public double  Nota2 { get; set; }
+        public double  Nota3 { get; set; }
 
-        private double _nota2;
-        public double Nota2
-        {
-            get { return _nota2; }
-            set { _nota2 = value; }
-        }
-
-        private double _nota3;
-        public double Nota3
-        {
-            get { return _nota3; }
-            set { _nota3 = value; }
-        }
         public Notas(double nota1, double nota2, double nota3)
         {
             Nota1 = nota1;
